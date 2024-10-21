1af641fdd97d9f348668f59b62e1cb8213df17df
Shivang <shivangtips@gmail.com>
2024-10-21 23:12:17 +0530

    Intial setup + tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-800 text-center font-bold "> BIZ WEB 2024</h1>
+    </>
+  );
+}
+
+export default App;
