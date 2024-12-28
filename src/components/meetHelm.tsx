4851ed4b3e1d92a63b290f8cdb37e31bc39df7c3
Tanmay Singh <tanmaysingh0632006@gmail.com>
2024-12-28 16:46:15 +0530

    updating changes in meet the helm

--- src/components/meetHelm.tsx
+++ src/components/meetHelm.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import image1 from '../assets/images/Screenshot 2024-12-24 225656.png';
-import image2 from '../assets/images/1723468189219.jpeg';
-import image3 from '../assets/images/Screenshot 2024-12-24 225827.png';
+import asthaImg from '../assets/images/astha_shetty.png';
+import aryanImg from '../assets/images/aryan_sharma.jpeg';
+import hrishitImg from '../assets/images/hrishit_yelchuri.png';
 
 
 const MeetTheHelm: React.FC = () => {
@@ -11,19 +11,19 @@ const MeetTheHelm: React.FC = () => {
             name: 'Astha Shetty',
             role: 'Secretary',
             description: 'A visionary leader with a knack for strategic thinking and innovation.',
-            image: image1,
+            image: asthaImg,
         },
         {
             name: 'Aryan Sharma',
             role: 'General Secretary',
             description: 'Passionate about fostering teamwork and ensuring smooth operations.',
-            image: image2,
+            image: aryanImg,
         },
         {
             name: 'Hrishit Yelchuri',
             role: 'General Secretary',
             description: 'Committed to driving growth and creating impactful initiatives.',
-            image: image3,
+            image: hrishitImg,
         },
     ];
 
@@ -37,7 +37,7 @@ const MeetTheHelm: React.FC = () => {
                 {members.map((member, index) => (
                     <div
                         key={index}
-                        className="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl transition-shadow duration-300"
+                        className="bg-white shadow-lg rounded-lg p-6 hover:shadow-xl hover:scale-105  duration-300"
                     >
                         <div className="bg-gradient-to-tr from-blue-50 to-gray-100">
                             <div className="w-40 h-40 mx-auto rounded-full border-4 border-white overflow-hidden">
@@ -50,9 +50,6 @@ const MeetTheHelm: React.FC = () => {
                             </div>
 
                         </div>
-                        {/* <h2 className="text-xl font-semibold text-gray-800 text-center">{member.name}</h2>
-            <p className="text-sm text-gray-600 text-center italic mb-4">{member.role}</p>
-            <p className="text-gray-700 text-center">{member.description}</p> */}
                         <div className="p-6 text-center">
                             <h2 className="text-2xl font-bold mb-2">{member.name}</h2>
                             <p className="text-sm font-semibold mb-4">{member.role}</p>
@@ -61,7 +58,7 @@ const MeetTheHelm: React.FC = () => {
                         <div className="p-4 bg-gray-100 flex justify-center gap-4">
                             <a
                                 href="#"
-                                className=":text-indigo-800 transition-colors"
+                                className="hover:text-indigo-800 transition-colors"
                             >
                                 LinkedIn
                             </a>
