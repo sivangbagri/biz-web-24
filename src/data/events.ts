de79c6ee111a33c52dc87af6e5c8ae64bf4075b4
Tanmay Singh <tanmaysingh0632006@gmail.com>
2025-03-30 16:10:16 +0530

    vertical images and enhanced description

--- src/data/events.ts
+++ src/data/events.ts
@@ -6,27 +6,33 @@ export const events: Event[] = [
     name: "Kotler's Maniac",
     image: '/events/KotlersManiac.png',
     summary: 'Step into the world of strategy, creativity, and marketing brilliance',
-    description: `# Kotler's Maniac
+    description: `# **Kotler's Maniac**
 
 Step into the world of strategy, creativity, and marketing brilliance with Kotler's Maniac, the flagship marketing event of the prestigious BASH (Business Associate Special Hours). Named after the legendary marketing guru Philip Kotler, this event is designed to challenge the sharpest minds in innovative marketing and branding.
 
 Let your ideas roar louder than the market noise—because this is where legends are made.
 
-## Guidelines
+## ‎ 
 
-* Eligibility: Students Only
+## **Guidelines:**
 
-## Rules and Regulations
+* Eligibility : Students Only
 
-1. Participants can participate in teams of 1 - 4
-2. There must not be technical issues from the participants' side
-3. The organizing committee shall not be held responsible for any such issues
-4. No disrespect shall be shown towards the judges
-5. Strict actions will be taken against those who break the discipline of the competition
-6. No entries will be entertained after the decided time limit
-7. The presentation should not contain any sort of disrespectful content towards any caste or religion
-8. Any sort of plagiarism will lead to immediate disqualification
-9. In any sort of dilemma during the competition, the decision of the judges will be final and binding`,
+## ‎
+
+## **Rules and Regulations:**
+
+# 1. Participants can participate in teams of 1 - 4
+# 2. There must not be technical issues from the participants' side
+# 3. The organizing committee shall not be held responsible for any such issues
+# 4. No disrespect shall be shown towards the judges
+# 5. Strict actions will be taken against those who break the discipline of the competition
+# 6. No entries will be entertained after the decided time limit
+# 7. The presentation should not contain any sort of disrespectful content towards any caste or religion
+# 8. Any sort of plagiarism will lead to immediate disqualification
+# 9. In any sort of dilemma during the competition, the decision of the judges will be final and binding
+## ‎
+***`,
     prizeMoney: 'INR 35,000',
     registrationLink: 'https://unstop.com/competitions/kotlers-maniac-bash-70-business-associates-special-hours-iit-bhu-1331735',
     problemStatementLink: '#',
@@ -48,34 +54,42 @@ Let your ideas roar louder than the market noise—because this is where legends
     name: 'Statistella',
     image: '/events/Statistella.png',
     summary: 'Analytics competition for data enthusiasts with real-world problems',
-    description: `# Statistella
+    description: `# **Statistella**
 
 Statistella is an exclusive competition for analytics enthusiasts as part of B.A.S.H 7.0, the flagship event of The Business Club, IIT BHU. The competition is structured in two challenging rounds to identify and reward individuals with exceptional analytical skills and visualization expertise.
 
-## Competition Format
+## ‎
 
-* Round 1: Machine Learning Challenge
-  * Solve real-world problems
-  * Demonstrate ML expertise
-  * Identify skilled candidates
+## **Competition Format:**
 
-* Round 2: Data Analytics & Visualization
-  * Showcase data analytics skills
-  * Present visualization expertise
-  * Final evaluation
+# ***- Round 1: Machine Learning Challenge***
+  # 1. Solve real-world problems
+  # 2. Demonstrate ML expertise
+  # 3. Identify skilled candidates
 
-## Guidelines
+# ***- Round 2: Data Analytics & Visualization***
+  # 1. Showcase data analytics skills
+  # 2. Present visualization expertise
+  # 3. Final evaluation
 
-1. Maximum team size: 4 participants
-2. Single team registration only
-3. Multiple entries not allowed
-4. Cross-college teams allowed
-5. Judges' decision is final
+## ‎
 
-## Perks of Participating
+## **Guidelines:**
 
-* Working on real world problem
-* Resume ready project`,
+# 1. Maximum team size: 4 participants
+# 2. Single team registration only
+# 3. Multiple entries not allowed
+# 4. Cross-college teams allowed
+# 5. Judges' decision is final
+
+## ‎
+
+## **Perks of Participating:**
+
+# 1. Working on real world problem
+# 2. Resume ready project
+## ‎
+***`,
     prizeMoney: 'INR 35,000+',
     registrationLink: 'https://unstop.com/competitions/statistella-bash-70-business-associates-special-hours-iit-bhu-1331831',
     problemStatementLink: '#',
@@ -95,27 +109,35 @@ Statistella is an exclusive competition for analytics enthusiasts as part of B.A
     name: 'Prodhive',
     image: '/events/Prodhive.png',
     summary: 'Product Case Study competition for aspiring Product Managers',
-    description: `# Prodhive
+    description: `# **Prodhive**
 
 Think you are the next visionary Product Manager? This will be a competition based on a problem statement, similar to those asked in company interviews, for great exposure. The teams will be expected to submit slides as a solution, based on which the top teams will be selected for the interview.
 
-## About Prodhive
+## ‎
+
+## **About Prodhive:**
 
 Prodhive is an Annual Product Case Study competition organized by the BASH, the flagship event of the business club IIT (BHU). Prodhive is an exciting opportunity to showcase your product skills. Dive into the user experience, examine the problem at hand, and explore the market to devise strategic enhancements.
 
-## About The Product Folks
+## ‎
+
+## **About The Product Folks:**
 
 Founded by passionate volunteers, The Product Folks fosters a vibrant community of individuals dedicated to product management excellence. Through a diverse range of learning resources, collaborative initiatives, and supportive mentorship, The Product Folks fosters continuous growth and propels its members towards achieving their professional goals.
 
-## Rules and Regulations
+## ‎
 
-1. Single team registration only
-2. Cross-specialization, cross-college, and cross-year teams allowed
-3. Further details shall be shared via email
-4. No entries after deadline
-5. No disrespectful content
-6. Plagiarism leads to disqualification
-7. Judges' decision is final`,
+## **Rules and Regulations:**
+
+# 1. Single team registration only
+# 2. Cross-specialization, cross-college, and cross-year teams allowed
+# 3. Further details shall be shared via email
+# 4. No entries after deadline
+# 5. No disrespectful content
+# 6. Plagiarism leads to disqualification
+# 7. Judges' decision is final
+## ‎
+***`,
     prizeMoney: 'INR 35,000+',
     registrationLink: 'https://unstop.com/competitions/prodhive-bash-70-business-associates-special-hours-iit-bhu-1336981',
     problemStatementLink: '#',
@@ -135,37 +157,47 @@ Founded by passionate volunteers, The Product Folks fosters a vibrant community 
     name: 'Portfolium - Innovate, Invest, Excel',
     image: '/events/Portfolium.png',
     summary: 'Put your financial skills to the test in an exciting portfolio management competition.',
-    description: `# Portfolium - Innovate, Invest, Excel
+    description: `# **Portfolium - Innovate, Invest, Excel**
 
 Put your financial skills to the test in Portfolium - Innovate, Invest, Excel, a competition designed for finance enthusiasts, students, and professionals. Start with a quiz to assess your knowledge, then move on to design a dynamic portfolio with a 1-year investment horizon. Unleash your creativity and market insights to build a portfolio that balances risk and reward, while adhering to real-world constraints.
 
-## Event Format
+## ‎
 
-### Round 1: Finance Quiz
+## **Event Format:**
+
+### ***- Round 1: Finance Quiz***
 A comprehensive quiz assessing knowledge of financial concepts, markets, and investment strategies.
 
-### Round 2: Portfolio Proposal Submission
+
+### ***- Round 2: Portfolio Proposal Submission***
 Participants submit a detailed portfolio in Excel format with the following constraints:
-* No limit on the number of assets (stocks, MF, ETFs, Gold, Cash, etc.), provided they are listed on NSE.
-* Stock selections must be from the NIFTY 750 Index.
-* Metrics required: Asset Allocation (%), Expected Returns, Risk Analysis, and Sectoral Diversification.
-* Portfolio rationale and strategy explanation.
+# 1. No limit on the number of assets (stocks, MF, ETFs, Gold, Cash, etc.), provided they are listed on NSE.
+# 2. Stock selections must be from the NIFTY 750 Index.
+# 3. Metrics required: Asset Allocation (%), Expected Returns, Risk Analysis, and Sectoral Diversification.
+# 4. Portfolio rationale and strategy explanation.
 
-### Round 3: Final Presentation (5-8 Slides)
+
+### ***- Round 3: Final Presentation (5-8 Slides)***
 Shortlisted teams present their portfolio to a panel of experts, focusing on:
-* Investment philosophy and strategy
-* Expected performance under various market conditions
-* Risk management measures and insights
+# 1. Investment philosophy and strategy
+# 2 Expected performance under various market conditions
+# 3. Risk management measures and insights
 
-## Rules & Guidelines
+## ‎
 
-1. Participation is open to individuals or teams of 2-3 members.
-2. Portfolio components must be listed on the NSE; stocks should be from the NIFTY 750 Index.
-3. Weightage will be given to innovation, rationale, and realistic projections.
-4. Judges' decision is final and binding.
+## **Rules & Guidelines:**
 
-## Rewards
-Prize worth ₹35,000`,
+# 1. Participation is open to individuals or teams of 2-3 members.
+# 2. Portfolio components must be listed on the NSE; stocks should be from the NIFTY 750 Index.
+# 3. Weightage will be given to innovation, rationale, and realistic projections.
+# 4. Judges' decision is final and binding.
+
+## ‎
+
+## **Rewards:**
+Prize worth ₹35,000
+## ‎
+***`,
     prizeMoney: 'INR 35,000+',
     registrationLink: 'https://unstop.com/competitions/portfolium-innovate-invest-excel-bash-70-business-associates-special-hours-iit-bhu-1339339',
     problemStatementLink: '#',
@@ -190,28 +222,30 @@ Prize worth ₹35,000`,
     description: `# Consultium
 
 Consultium is an esteemed consulting case competition that challenges participants to tackle real-world business challenges and devise innovative solutions in a high-pressure, fast-paced environment. The competition format is designed to simulate the rigor and demands of a consulting career, requiring participants to work collaboratively in teams to present their solutions effectively to a panel of expert judges.
+## ‎
+## **Rules:**
 
-## Rules
+# 1. Participants can participate in teams of 1-4.
+# 2. Participants' devices must have a speaker, mic, and camera in proper working conditions.
+# 3. The organizing committee shall not be held responsible for technical issues from participants' side.
+# 4. No disrespect shall be shown towards the judges.
+# 5. Strict actions will be taken against those who break the discipline of the competition.
+# 6. No entries will be entertained after the decided time limit.
+# 7. The presentation should not contain disrespectful content towards any caste or religion.
+# 8. Any sort of plagiarism will lead to immediate disqualification.
+# 9. In any sort of dilemma during the competition, the decision of the judges will be final and binding.
+## ‎
+## **Important Dates:**
 
-1. Participants can participate in teams of 1-4.
-2. Participants' devices must have a speaker, mic, and camera in proper working conditions.
-3. The organizing committee shall not be held responsible for technical issues from participants' side.
-4. No disrespect shall be shown towards the judges.
-5. Strict actions will be taken against those who break the discipline of the competition.
-6. No entries will be entertained after the decided time limit.
-7. The presentation should not contain disrespectful content towards any caste or religion.
-8. Any sort of plagiarism will lead to immediate disqualification.
-9. In any sort of dilemma during the competition, the decision of the judges will be final and binding.
-
-## Important Dates
-
-* Registration Deadline: 10 Jan 2025, 11:59 PM IST
-* Round 1 Deadline: 10 Jan 2025, 11:59 PM IST
-* Round 2 Deadline: 15 Jan 2025, 11:59 PM IST
-* Final Round: 18 Jan 2025, 10:00 AM IST
-
-## Rewards
-Prize worth ₹35,000`,
+# - Registration Deadline: 10 Jan 2025, 11:59 PM IST
+# - Round 1 Deadline: 10 Jan 2025, 11:59 PM IST
+# - Round 2 Deadline: 15 Jan 2025, 11:59 PM IST
+# - Final Round: 18 Jan 2025, 10:00 AM IST
+## ‎
+## **Rewards:**
+# - Prize worth ₹35,000
+## ‎
+***`,
     prizeMoney: 'INR 35,000+',
     registrationLink: 'https://unstop.com/competitions/consultium-bash-70-business-associates-special-hours-iit-bhu-1338981',
     problemStatementLink: '#',
