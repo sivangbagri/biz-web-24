abba64c11c9f3f54e0d3f7023555508dc8480aa2
Tanmay Singh <tanmaysingh0632006@gmail.com>
2025-04-19 19:37:42 +0530

    navbar issue resolve

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const navbarRef = useRef(null);
+  const navbarRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
     const handleScroll = () => {
